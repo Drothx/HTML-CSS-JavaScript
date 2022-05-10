920eaadb355122b7f55c7f3c9645b6e2ec6332ce
Drothx <64139713+Drothx@users.noreply.github.com>
2022-05-11 00:10:17 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,155 +1,48 @@
-<script>
-      
-    document.getElementById("titre_un").onmouseover = function() {mouseOver()};
-    document.getElementById("titre_un").onmouseout = function() {mouseOut()};
-    
+const moduleHttp = require('http');
 
-  function mouseOver() {
-    document.getElementById("titre_un").style.color = "red";
-    document.getElementById("titre_un").style.backgroundColor="yellow";
-    document.getElementById("titre_un").style.fontSize = "34px";
-  }
+const port = 8070;
+const host = '127.0.0.1';
 
-  function mouseOut() {
-    document.getElementById("titre_un").style.color = "black";
-    document.getElementById("titre_un").style.backgroundColor="";
-    document.getElementById("titre_un").style.fontSize = "24px";
-  }
+//console.log('Module http : ', moduleHttp);
 
-    </script>
+const monServeur = moduleHttp.createServer();
 
+/*monServeur.listen(port, host, function() */
+function serveurLance()
+    {   
+        console.log(`Serveur démarré à l'adresse http://${host}:${port}`);
+    };
 
- const killingRobot = {
-   ref: 'KR-MACH001',
-   codeName: 'Pedro',
-   energySource: 'electricity',
-   battery: {
-     mAhCapacity: 1000000,
-     material: 'adamantium',
-     technology: 'high-power lithium-ion'
-   },
-   dimensions: {
-     heightInCm: 450,
-     widthInCm: 190,
-     weightInKg: 3600
-   },
-   weaponry: [
-     'laser beam machine guns',
-     'nuclear rocket launchers',
-     'adamantium sword',
-     'adamantium spear',
-     'H-Bomb',
-     'electromagnetic impulse canon',
-     'sulfuric acid high-pressure canon'
-   ],
-   tools: [
-     'emergency self-repair nanotechnology',
-     'advanced machine learning capabilities',
-     'high capacity solar panels',
-     'adamantium shell coating',
-     'adamantium wings',
-     'adamantium solar powered engines'
-   ],
-   abilities: {
-     canWipeOutMankindFromTheFaceOfTheEarth: true,
-     lovesAnimals: true,
-     hasAchievedUltraInstinct: function() {
-       if (this.battery.mAhCapacity >= 1000000) {
-        alert("Adieu monde cruel"); 
-        return true;
-       } else {
-         return false;
-       }
-     },
-     canFly: true,
-     canSwim: false,
-   },
- };
+//écoute d'évènement
+monServeur.on('request', function(request, response)
+    {
+        console.log('request.url: ', request.url);
+        const message = `<h1> Titre 1</h1>\n </br><hr></br><p> Heureux de vous revoir !</p>`;
+        response.writeHead
+        (
+            200,
+            "C'est OK",
+            {
+                'Content-Type': 'text/html; charset=utf8',
+                'Content-Lenght': Buffer.byteLength(message),
+            }
+        );
+    //Puis on écrit la reponse
+    response.write(message, function()
+    {
+        response.end();
+    })
+});  //quand il va recevoir une request, il va lancer un callback
 
+monServeur.listen(port, host, serveurLance);
 
+// Nous allons écrire une boucle qui va faire plusieurs console.log...
 
- function exercice4( arg1, arg2 ) {
-
-    if (typeof arg1 === 'number' && typeof arg2 === 'number') {
-    
-    tmp = arg1 + arg2;
-    
-    return tmp * 3;
-    
-    }
-    
-    else if (typeof arg1 === 'string' && typeof arg2 === 'string') {
-    
-    tmp = arg1 + arg2;
-    
-    return tmp;
-    
-    }
-    
-    else {
-    
-    return {
-    
-    'nombres': 'Il absolument faut 2 nombres !',
-    
-    'string': 'Il faut absolument 2 chaînes de caractère !'
-    
-    }
-    
-    }
-    
-    }
-
-
-
-
-function exercice5 ( tab ) {
-  let tmp = 0;
-  for (let i = 0; i < tab.length; i++) {
-    if (typeof tab[i] === 'number') {
-      tmp = tmp + tab[i];
-    }
-  }
-  return tmp;
-}
-
-
-
-  <body>
-    <div class="main-container">
-
-      <section id="articles-section">
-        <p class="article"></p>
-        <p class="article"></p>
-        <p class="article"></p>
-      </section>
-    
-      <section id="images-section">
-        <img src="" alt="Une image aléatoire" />
-        <button type="button" onclick="pickImgSrc()">Change Image !</button>
-      </section>
-
-    </div>
-  </body>
- *
- * **/
-
-
-document.getElementById('p1').innerHTML = 
-<body>
-    <div class="main-container">
-
-      <section id="articles-section">
-        <p class="article"></p>
-        <p class="article"></p>
-        <p class="article"></p>
-      </section>
-    
-      <section id="images-section">
-        <img src="" alt="Une image aléatoire" />
-        <button type="button" onclick="pickImgSrc()">Change Image !</button>
-      </section>
-
-    </div>
-  </body>];
-
+/*for (let compteur = 1; compteur <= 100; compteur++) //++ peut aussi s'ecrire "+= 1"
+    {
+        //Le 1er param de la boucle for est le compteur
+        //Le 2eme oaral est la condition de sortie de boucle
+        //Le 3eme param est l'incrementation du compteur qui va s'effectuer a chaque tour de boucle
+        console.log(`Bonjour ${compteur} fois !`); //Alt Gr + 7 pour faire des back quotes
+        
+    };*/
